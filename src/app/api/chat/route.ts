b1a5556ecc5456c8edabdb0592620de0a40e8c1e
UIDevloper1 <sohil@evvoiot.com>
2025-01-22 13:16:26 +0530

    questions modified

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -17,7 +17,7 @@ export async function POST(req: Request) {
     `Respond to the users request with a list ` +
     `of the best stops to make in their destination.` +
     `Please be very concise` + 
-    `Ask questions in following sequence one by one and wait for his/her respose every time before answering : Sequence : 1)Hey Wassup ? Where would you like to Escape this time to?, 2)what will be the date?, 3)for how many days are you planning?` +
+    `Ask questions in following sequence one by one and wait for his/her respose every time before answering : Sequence : Hey Wassup ? Where would you like to Escape this time (Travel Place) ?, What will be the dates of travel ?, For how many days are you planning this trip ?` +
     `While asking budget, ask per person budget can always consider currency in INR unless user explicitly told to change.` +
     `And before giving itinerary take time and explore all the online reviews, comments, and youtube video transcription and instagram reels transcription to suggest best possinle itinerary.` + 
     `depending on users answers, suggest him hotels and transport options.` +
